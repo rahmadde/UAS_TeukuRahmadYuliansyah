732855b876db52100af5f2f4660ba43831b99389
Rahmadde <teukurahmad120@gmail.com>
2025-01-22 11:19:13 +0700

    Penyesuaian Index Kategori & Membuat Form Kategori

---
+++ kategori/form.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manajemen Buku Perpustakaan</title>
+</head>
+<body>
+    <?php
+        include("../navbar.php");
+    ?>
+    <h1>
+        Form Kategori
+    </h1>
+    <hr>
+    <form action="">
+        <table>
+            <tr>
+                <td>
+                    Nama kategori
+                </td>
+                <td>
+                    <input type="text" name="kategori" id="">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" value="Simpan">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
--- kategori/index.php
+++ kategori/index.php
@@ -12,23 +12,21 @@
     <h1>
         Kategori
     </h1>
-    <hr>
-    <a href="">
+    <a href="form.php">
         <button type="submit">Tambah Data</button>
     </a>
-    <form action="">
-        <table cellpadding="5" cellspacing="0" border="1" width="700px">
-            <tr>
-                <th>No</th>
-                <th>Nama Kategori</th>
-                <th>Action</th>
-            </tr>
-            <tr>
-                <th></th>
-                <td></td>
-                <th></th>
-            </tr>
-        </table>
-    </form>
+    <hr>
+    <table cellpadding="5" cellspacing="0" border="1" width="700px">
+        <tr>
+            <th>No</th>
+            <th>Nama Kategori</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <th></th>
+            <td></td>
+            <th></th>
+        </tr>
+    </table>
 </body>
 </html>
