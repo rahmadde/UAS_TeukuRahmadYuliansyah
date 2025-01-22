4bfff688ad748b1c58a9717de6ac5d7a4a5b2ba8
Rahmadde <teukurahmad120@gmail.com>
2025-01-22 11:08:29 +0700

    Membuat Koneksi

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $databse = "perpustakaan";
+
+    $koneksi = mysqli_connect($host, $username, $password, $databse);
+?>
